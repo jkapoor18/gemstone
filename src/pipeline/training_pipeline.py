cc02722afa1367b582bb1529ccc4f8dd46e88663
jkapoor18 <jyotikpr999@gmail.com>
2023-04-16 20:32:01 +0530

    pipeline

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -3,7 +3,6 @@ import sys
 from src.logger import logging
 from src.exception import customException
 import pandas as pd
-
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
 from src.components.model_trainer import ModelTrainer
@@ -12,8 +11,8 @@ if __name__=='__main__':
     obj=DataIngestion()
     train_data_path,test_data_path=obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    train_arr,test_arr=data_transformation.initate_data_transformation(train_data_path,test_data_path)
+    train_arr,test_arr,_=data_transformation.initaite_data_transformation(train_data_path,test_data_path)
 
     model_trainer=ModelTrainer()
     model_trainer.initiate_model_trainer(train_arr,test_arr)
-
+    
